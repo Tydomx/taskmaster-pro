138e7cf807327cbdaed08e9d951b24ac810d4d15
Tydomx <michaelthai16@gmail.com>
2022-04-13 15:05:53 -0500

    UX enhancements

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -52,7 +52,7 @@ var auditTask = function(taskEl) {
   // get date from task element
   var date = $(taskEl).find("span").text().trim();
   // to ensure element is getting to the function
-  console.log(date);
+  // console.log(date);
 
   // convert to moment object at 5:00pm
   var time = moment(date, "L").set("hour", 17);
@@ -70,6 +70,7 @@ var auditTask = function(taskEl) {
   else if (Math.abs(moment().diff(time, "days")) <= 2) {
     $(taskEl).addClass("list-group-item-warning");
   }
+  console.log(taskEl);
 };
 
 // enabe draggable/sortable feature on list-group elements
@@ -80,16 +81,18 @@ $(".card .list-group").sortable({
   tolerance: "pointer",
   helper: "clone",
   activate: function(event, ui) {
-    console.log(ui);
+    $(this).addClass(".dropover");
+    $("bottom-trash").addClass(".bottom-trash-drag");
   },
   deactivate: function(event, ui) {
-    console.log(ui);
+    $(this).addClass(".dropover");
+    $(".bottom-trash").addClass(".bottom-trash-drag");
   },
   over: function(event) {
-    console.log(event);
+    $(event.target).addClass("dropover-active");
   },
   out: function(event) {
-    console.log(event);
+    $(event.target).addClass("dropover-active");
   },
   update: function() {
     // array to store the task data in
@@ -129,12 +132,13 @@ $("trash").droppable({
   tolerance: "touch",
   drop: function(event, ui) {
     ui.draggable.remove(); // drag item to remove it <--
+    $(".bottom-trash").addClass(".bottom-trash-active");
   },
   over: function(event, ui) {
-    console.log(ui);
+    $(".bottom-trash").addClass(".bottom-trash-active");
   },
   out: function(event, ui) {
-    console.log(ui);
+    $(".bottom-trash").addClass(".bottom-trash-active");
   }
 });
 
@@ -157,7 +161,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -306,4 +310,8 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
-
+setInterval(function(){
+  $(".card list-group-item").each(function(index, el){
+    auditTask(el);
+  })
+}, (1000*60) *30); 
